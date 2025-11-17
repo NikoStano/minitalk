6cbabc549b593742b8718b31cdafdc17e8aa7efa
NikoStano <nistanoj@f1br7s17.clusters.42paris.fr>
2025-11-17 05:08:53 +0100

    Update

--- utils.c
+++ utils.c
@@ -6,24 +6,15 @@
 /*   By: nistanoj <nistanoj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/30 00:54:22 by nistanoj          #+#    #+#             */
-/*   Updated: 2025/09/30 01:06:07 by nistanoj         ###   ########.fr       */
+/*   Updated: 2025/11/17 05:06:56 by nistanoj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/minitalk.h"
 
-void	ft_putchar(char c)
-{
-	write(1, &c, 1);
-}
-
 void	ft_putstr(char *str)
 {
-	int	i;
-
-	i = 0;
-	while (str[i])
-		ft_putchar(str[i++]);
+	write(1, str, ft_strlen(str));
 }
 
 int	ft_strlen(char *str)
@@ -38,7 +29,10 @@ int	ft_strlen(char *str)
 
 void	ft_putnbr(int nb)
 {
+	char	c;
+
 	if (nb / 10)
 		ft_putnbr(nb / 10);
-	ft_putchar(nb % 10 + 48);
+	c = nb % 10 + 48;
+	write(1, &c, 1);
 }
